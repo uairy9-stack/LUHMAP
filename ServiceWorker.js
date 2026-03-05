4c6dfb69ea81315fdf1ad1f59abb0ef4be61c244
uairy9-stack <uairy9@gmail.com>
2026-03-06 07:22:22 +0900

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-MAP-1.0";
 const contentToCache = [
-    "Build/USHS13.loader.js",
-    "Build/USHS13.framework.js.unityweb",
-    "Build/USHS13.data.unityweb",
-    "Build/USHS13.wasm.unityweb",
+    "Build/USHS14.loader.js",
+    "Build/USHS14.framework.js.unityweb",
+    "Build/USHS14.data.unityweb",
+    "Build/USHS14.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
